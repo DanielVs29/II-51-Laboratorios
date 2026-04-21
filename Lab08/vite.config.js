51266040db315d387ff40d0d064f565d776c9fa0
Daniel Vargas Soto <danieljvargas29@gmail.com>
2026-04-20 20:46:18 -0600

    Lab08 visualizacion

--- Lab08/vite.config.js
+++ Lab08/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: '/II-51-Laboratorios/Lab08/dist/',
+  base: '/II-51-Laboratorios/Lab08/'
 })
